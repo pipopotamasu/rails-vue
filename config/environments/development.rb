f9899e6d9ad023e9b354cec26feeb98b285ff16a
大和村上 <yamatomurakami@daiwamurakami-no-MacBook-Pro.local>
2017-06-14 07:53:50 +0900

    delete Fuyōna
    Unnecessary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.x.webpacker[:dev_server_host] = 'http://localhost:8080'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
