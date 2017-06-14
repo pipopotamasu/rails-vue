1ac5a0305842df7737a03fa823891bad90df1472
大和村上 <yamatomurakami@daiwamurakami-no-MacBook-Pro.local>
2017-06-15 07:46:34 +0900

    copy writing

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,16 +7,15 @@
 import Vue from 'vue'
 import App from './app.vue'
 
-// register the grid component
 Vue.component('demo-grid', {
   template: '#grid-template',
   props: {
     data: Array,
     columns: Array,
-    filterKey: String
+    filterKey: String,
   },
   data: function () {
-    var sortOrders = {}
+    let sortOrders = {}
     this.columns.forEach(function (key) {
       sortOrders[key] = 1
     })
@@ -27,34 +26,37 @@ Vue.component('demo-grid', {
   },
   computed: {
     filteredData: function () {
-      var sortKey = this.sortKey
-      var filterKey = this.filterKey && this.filterKey.toLowerCase()
-      var order = this.sortOrders[sortKey] || 1
-      var data = this.data
+      let sortKey = this.sortKey
+      let filterKey = this.filterKey && this.filterKey.toLowerCase()
+      let order = this.sortOrders[sortKey] || 1
+      let data = this.data
       if (filterKey) {
-        data = data.filter(function (row) {
-          return Object.keys(row).some(function (key) {
+        // return filter data by using indexOf
+        data = data.filter((row) => {
+          return Object.keys(row).some((key) => {
             return String(row[key]).toLowerCase().indexOf(filterKey) > -1
           })
         })
       }
       if (sortKey) {
-        data = data.slice().sort(function (a, b) {
+        data = data.slice().sort((a, b) => {
           a = a[sortKey]
           b = b[sortKey]
           return (a === b ? 0 : a > b ? 1 : -1) * order
         })
       }
+
       return data
     }
   },
   filters: {
+    // capitalize -> start initial char by the capital. ex) name -> Name
     capitalize: function (str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
     }
   },
-  methods: {
-    sortBy: function (key) {
+  mthods: {
+    sortBy: function(key) {
       this.sortKey = key
       this.sortOrders[key] = this.sortOrders[key] * -1
     }
@@ -75,31 +77,3 @@ var demo = new Vue({
     ]
   }
 })
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-
-// import Vue from 'vue/dist/vue.esm'
-// import App from './app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
