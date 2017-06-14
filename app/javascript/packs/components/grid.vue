254c03d18fd1dc73e91c1d73f93439f2663daa25
大和村上 <yamatomurakami@daiwamurakami-no-MacBook-Pro.local>
2017-06-15 08:23:31 +0900

    split component

--- app/javascript/packs/components/grid.vue
+++ app/javascript/packs/components/grid.vue
@@ -1,13 +1,6 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> and
-// <%= stylesheet_pack_tag 'hello_vue' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
+<script>
 
-import Vue from 'vue'
-import App from './app.vue'
-
-Vue.component('demo-grid', {
+export default {
   template: '#grid-template',
   props: {
     data: Array,
@@ -61,19 +54,5 @@ Vue.component('demo-grid', {
       this.sortOrders[key] = this.sortOrders[key] * -1
     }
   }
-})
-
-// bootstrap the demo
-var demo = new Vue({
-  el: '#demo',
-  data: {
-    searchQuery: '',
-    gridColumns: ['name', 'power'],
-    gridData: [
-      { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jackie Chan', power: 7000 },
-      { name: 'Jet Li', power: 8000 }
-    ]
-  }
-})
+}
+</script>
---
+++ app/javascript/packs/grid.js
@@ -0,0 +1,25 @@
+/* eslint no-console: 0 */
+// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> and
+// <%= stylesheet_pack_tag 'hello_vue' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+// All it does is render <div>Hello Vue</div> at the bottom of the page.
+
+import Vue from 'vue'
+import Grid from './components/grid.vue'
+
+Vue.component('demo-grid', Grid)
+
+// bootstrap the demo
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    searchQuery: '',
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ]
+  }
+})
