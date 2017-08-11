3a48cd5426d1ca59e34a60a8471f507d5c4ba828
pipopotamasu <ec0156hx@gmail.com>
2017-08-12 02:02:20 +0900

    autofocus

--- app/javascript/packs/components/vuex_practice/card.vue
+++ app/javascript/packs/components/vuex_practice/card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card-body">
-      <input v-show="editing" @blur="doneEdit" @keyup.enter="doneEdit" type="text" size="18" :value="card.name">
+      <input v-show="editing" @blur="doneEdit" v-focus @keyup.enter="doneEdit" type="text" size="18" :value="card.name">
       <span v-show="!editing" @click="onEditing">{{ card.name }}</span>
       <i v-show="!editing" @click="DELETE_CARD(card)" class="glyphicon glyphicon-trash pull-right delete-card"></i>
     </div>
@@ -15,6 +15,13 @@ export default {
   data () {
     return {
         editing: false
+    }
+  },
+  directives: {
+    focus: {
+      update: function(el) {
+        el.focus();
+      }
     }
   },
   methods: {
